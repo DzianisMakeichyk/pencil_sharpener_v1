e3ddc79cac2f2c58c1b87f7ef8bff0ab6b2f6aa1
dzianis <dzianis.makeichyk@gmail.com>
2018-04-16 14:44:23 +0200

    add burger css

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,8 +2,11 @@
     <div
       class="menu-wrapper"
       @click="MenuToggle"
-      v-bind:class="{ 'is-active': this.$store.state.isOpenMenu}"
+      :class="{ 'is-active': this.$store.state.isOpenMenu}"
     >
+        <div class="burger" :class="{ 'clicked': this.$store.state.isOpenMenu}">
+            <span></span>
+        </div>
         <div class="slideout-opener">
             {{this.$store.state.isOpenMenu}}
         </div>
@@ -28,4 +31,71 @@ export default {
   .menu-wrapper {
     cursor: pointer;
   }
+
+  .burger {
+    position: fixed;
+    top: 10px;
+    right: 20px;
+    width: 50px;
+    height: 50px;
+    border-radius: 4px;
+    z-index: 10;
+
+    span {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      user-select: none;
+
+      &:before,
+      &:after {
+        content: '';
+        position: absolute;
+      }
+
+      &:before {
+        top: -6px;
+      }
+
+      &:after {
+        top: 6px;
+      }
+    }
+
+    &.clicked {
+      span {
+        background-color: transparent;
+
+        &:before
+        &:after {
+          background-color: $white;
+        }
+
+        &:before {
+          transform: translateY(6px) rotate(45deg);
+        }
+
+        &:after {
+          transform: translateY(-6px) rotate(-45deg);
+        }
+      }
+
+      &.clicked span::before {
+
+      }
+    }
+  }
+
+  .burger span,
+  .burger span::before,
+  .burger span::after {
+      display: block;
+      width: 25px;
+      height: 2px;
+      background-color: $black;
+      outline: 1px solid transparent;
+      transition-property: background-color, transform;
+      transition-duration: 0.3s;
+  }
+
 </style>
