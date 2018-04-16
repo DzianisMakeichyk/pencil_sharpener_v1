c1c49b37afdf2a2e731d18939172ea8a3898a986
dzianis <dzianis.makeichyk@gmail.com>
2018-04-16 15:25:02 +0200

    add typed

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,35 @@
 <template>
     <div>
-        hello me
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <vue-typer
+          :text='["Arya Stark","Jon Snow","Daenerys Targaryen","Melisandre","Tyrion Lannister"]'
+          :repeat='Infinity'
+          :shuffle='false'
+          initial-action='typing'
+          :pre-type-delay='70'
+          :type-delay='70'
+          :pre-erase-delay='2000'
+          :erase-delay='250'
+          erase-style='select-all'
+          :erase-on-complete='false'
+          caret-animation='blink'
+        ></vue-typer>
     </div>
 </template>
 
 <script>
+import { VueTyper } from 'vue-typer'
+
 export default {
-  name: 'Index'
+  name: 'Index',
+  components: {
+    VueTyper
+  }
 }
 </script>
 
