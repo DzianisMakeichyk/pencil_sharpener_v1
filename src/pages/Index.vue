a4266a22eff6fc3fc9c779413c6bfa802d102a92
dzianismakeichyk <dzianis.makeichyk@gmail.com>
2018-04-17 21:11:52 +0200

    add typejs

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,75 +2,32 @@
     <div>
         <div class="hero-wrapper">
             <h2 class="hero-name">
-                Pencil to&nbsp;
+                Pencil <span v-if="this.$store.state.windowWidth >= 767">Sharpener </span>to
+                <span class="typed-me" id="typed"></span>
             </h2>
-            <span
-              class="txt-rotate"
-              data-period="2000"
-              data-rotate='[ "nerdy nerdy.nerdy nerdy,nerdy nerdy nerdy nerdy nerdy nerdynerdy nerdy nerdy simple.", "pure JS.", "pretty.", "fun!" ]'>
-            </span>
         </div>
     </div>
 </template>
 
 <script>
+import Typed from 'typed.js'
 
 export default {
   name: 'Index',
+  data () {
+    return {
+      typedOptions: ['World', 'Mundo', 'Mondo', 'Svět', 'Wellt']
+    }
+  },
   mounted () {
-    let TxtRotate = function (el, toRotate, period) {
-      toRotate = toRotate
-      this.el = el
-      this.loopNum = 0
-      this.period = parseInt(period, 10) || 2000
-      this.txt = ''
-      this.tick()
-      this.isDeleting = false
-    console.log(this.toRotate)
-    }
-
-    TxtRotate.prototype.tick = function () {
-      let i = this.loopNum % this.toRotate.length
-      let fullTxt = this.toRotate[i]
-      if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1)
-      } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1)
-      }
-      this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>'
-
-      let that = this
-      let delta = 300 - Math.random() * 100
-
-      if (this.isDeleting) { delta /= 2 }
-
-      if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period
-        this.isDeleting = true
-      } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false
-        this.loopNum++
-        delta = 500
-      }
-
-      setTimeout(function () {
-        that.tick()
-      }, delta)
-    }
-
-    let elements = document.getElementsByClassName('txt-rotate')
-    for (let i = 0; i < elements.length; i++) {
-      let toRotate = elements[i].getAttribute('data-rotate')
-      let period = elements[i].getAttribute('data-period')
-      if (toRotate) {
-        TxtRotate(elements[i], JSON.parse(toRotate), period)
-      }
-    }
-    // INJECT CSS
-    let css = document.createElement('style')
-    css.type = 'text/css'
-    css.innerHTML = '.txt-rotate > .wrap { border-right: 0.08em solid #666 }'
-    document.body.appendChild(css)
+    const typed = new Typed('#typed', {
+      strings: this.typedOptions,
+      typeSpeed: 100,
+      backSpeed: 50,
+      loop: true,
+      backDelay: 2000
+    })
+    console.log(typed)
   }
 }
 </script>
@@ -79,28 +36,20 @@ export default {
 <style lang="scss">
   .hero-wrapper {
       position: absolute;
-      /*display: inline-flex;*/
-      /*align-content: flex-start;*/
-      /*justify-content: center;*/
-      /*width: 100%;*/
       bottom: 15%;
       left: 5%;
       letter-spacing: 1px;
 
       .hero-name {
+          display: inline;
           width: 140px;
           margin: 0 auto 0 0;
           font-size: 30px;
           font-family: 'GT-Haptik-Thin';
       }
-      /* Typed */
-      .vue-typer {
-          font-size: 30px;
-          font-family: 'GT-Haptik-Thin';
-
-          .custom.char {
-              color: $gray;
-          }
+      .typed-me {
+          color: $gray;
+          font-family: 'GT-Haptik-Lazer';
       }
   }
 </style>
